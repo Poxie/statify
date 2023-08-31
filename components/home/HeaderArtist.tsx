9b797bbc0c2856f627cf9616ed10fd0e9b682f13
Poxie <poxen12@hotmail.com>
2023-08-31 23:13:22 +0200

    Made artist icons togglable

--- components/home/HeaderArtist.tsx
+++ components/home/HeaderArtist.tsx
@@ -17,9 +17,10 @@ export default function HeaderArtist({
     right?: string;
 }) {
     const currentArtistId = useSearchParams().get('a');
+    const isActive = currentArtistId === id;
     return(
         <Link
-            href={`/?a=${id}`} 
+            href={isActive ? '/' : `/?a=${id}`} 
             className={`absolute w-16 aspect-square last:translate-y-[260%] ${left ? '-translate-x-[160%]' : 'translate-x-[160%]'} lg:translate-x-0 last:lg:translate-y-0`}
             style={{
                 top,
@@ -28,7 +29,7 @@ export default function HeaderArtist({
             }}
         >
             <Image 
-                className={`duration-500ms object-cover w-full h-full transition-[border-radius] ${currentArtistId === id ? 'rounded-xl' : 'rounded-[30px] hover:rounded-3xl'}`}
+                className={`duration-500ms object-cover w-full h-full transition-[border-radius] ${isActive ? 'rounded-xl' : 'rounded-[30px] hover:rounded-3xl'}`}
                 src={image}
                 width={100}
                 height={100}
