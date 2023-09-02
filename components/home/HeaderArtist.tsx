cdbf07dcb4a31f9b4b2fd55c0f9772332bd87917
Poxie <poxen12@hotmail.com>
2023-09-02 17:32:19 +0200

    Added parallax/translation to header artists

--- components/home/HeaderArtist.tsx
+++ components/home/HeaderArtist.tsx
@@ -2,7 +2,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useRef } from "react";
 
+const MIN_PARALLAX = 4;
+const MAX_PARALLAX = 18;
+const MAX_TRANSLATION_X = 5;
+const MAX_TRANSLATION_Y = 2;
 export default function HeaderArtist({
     id,
     image,
@@ -18,6 +23,35 @@ export default function HeaderArtist({
 }) {
     const currentArtistId = useSearchParams().get('a');
     const isActive = currentArtistId === id;
+    
+    const ref = useRef<HTMLAnchorElement>(null);
+
+    useEffect(() => {
+        const randomParallaxShift = Math.random() * (MAX_PARALLAX - MIN_PARALLAX) + MIN_PARALLAX;
+        console.log(randomParallaxShift);
+        const onMouseMove = (e: MouseEvent) => {
+            if(!ref.current) return;
+
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+            const mouseXPos = e.clientX;
+            const mouseYPos = e.clientY;
+
+            const widthPart = width / 2;
+            const relativeXPos = widthPart - mouseXPos;
+            const translateXPercentage = relativeXPos / widthPart;
+
+            const heightPart = height / 2;
+            const relativeYPart = heightPart - mouseYPos;
+            const translateYPercentage = relativeYPart / heightPart;
+
+            ref.current.style.transform = `translateY(${translateYPercentage * -(MAX_TRANSLATION_Y + randomParallaxShift)}px) translateX(${translateXPercentage * -(MAX_TRANSLATION_X + randomParallaxShift)}px)`;
+        }
+
+        window.addEventListener('mousemove', onMouseMove);
+        return () => window.removeEventListener('mousemove', onMouseMove);
+    }, []);
+
     return(
         <Link
             scroll={false}
@@ -28,6 +62,7 @@ export default function HeaderArtist({
                 left,
                 right,
             }}
+            ref={ref}
         >
             <Image 
                 className={`duration-500ms object-cover w-full h-full transition-[border-radius] ${isActive ? 'rounded-xl' : 'rounded-[30px] hover:rounded-3xl'}`}
