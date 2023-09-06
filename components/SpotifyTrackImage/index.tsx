416edf4f4854bd70da494f8f3305537630560654
Poxie <poxen12@hotmail.com>
2023-09-06 17:16:35 +0200

    Cleaned messy classname

--- components/SpotifyTrackImage/index.tsx
+++ components/SpotifyTrackImage/index.tsx
@@ -4,6 +4,7 @@ import { SpotifyTrack } from "@/types";
 import { PlayIcon } from "@/assets/icons/PlayIcon";
 import { PauseIcon } from "@/assets/icons/PauseIcon";
 import { usePreviewPause } from "@/hooks/usePreviewPause";
+import clsx from "clsx";
 
 export default function SpotifyTrackImage(props: SpotifyImageProps & {
     track: SpotifyTrack;
@@ -29,7 +30,11 @@ export default function SpotifyTrackImage(props: SpotifyImageProps & {
                 disabled={!canBePreviewed}
                 onClick={handleClick}
                 aria-label={isPausable ? canBePreviewed ? `Play preview for ${track.name}` : `Preview for ${track.name} is not available` : `Pause preview for ${track?.name}`}
-                className={`absolute w-full h-full left-0 top-0 flex items-center justify-center bg-secondary bg-opacity-75 opacity-0 group-hover:opacity-100 ${isPausable ? 'opacity-100' : ''} duration-300 transition-opacity ${!canBePreviewed ? 'after:bg-t-secondary after:rounded-full after:h-[1.35rem] after:w-[.18rem] after:rotate-45 after:absolute after:left-[50%+.25rem] after:-translate-x-2/4' : 'focus:opacity-100'}`}
+                className={clsx(
+                    "absolute w-full h-full left-0 top-0 flex items-center justify-center bg-secondary bg-opacity-75 opacity-0 group-hover:opacity-100 duration-300 transition-opacity",
+                    !canBePreviewed ? 'after:bg-t-secondary after:rounded-full after:h-[1.35rem] after:w-[.18rem] after:rotate-45 after:absolute after:left-[50%+.25rem] after:-translate-x-2/4' : 'focus:opacity-100',
+                    isPausable && 'opacity-100',
+                )}
             >
                 {isPausable ? (
                     <PauseIcon className="w-7" />
