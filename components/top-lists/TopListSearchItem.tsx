cefe52a6870117342e4890622b1bead180e7fec0
Poxie <poxen12@hotmail.com>
2023-09-11 01:53:26 +0200

    Color changes to top list search

--- components/top-lists/TopListSearchItem.tsx
+++ components/top-lists/TopListSearchItem.tsx
@@ -5,7 +5,7 @@ export default function TopListSearchItem({ text, onSelect }: {
     return(
         <li key={text}>
             <button
-                className="w-full text-left text-lg py-2 px-3 rounded-lg transition-colors hover:bg-tertiary"
+                className="w-full text-left text-lg py-2 px-3 rounded-lg duration-300 transition-colors text-secondary hover:bg-tertiary hover:text-primary"
                 onClick={onSelect}
             >
                 {text}
