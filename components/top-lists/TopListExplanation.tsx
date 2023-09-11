efbd052bedc29ac10e0a562196603073a4d0b1c2
Poxie <poxen12@hotmail.com>
2023-09-11 23:27:54 +0200

    Increased text to background contrast

--- components/top-lists/TopListExplanation.tsx
+++ components/top-lists/TopListExplanation.tsx
@@ -8,7 +8,7 @@ export default function TopListExplanation() {
                 <p className="text-lg text-secondary mt-2 mb-8">
                     It is currently not possible to get top tracks by country, or globally for that matter. Therefore, we have to be a little sneaky to achieve this, which comes with its pros and cons. We simple search for playlists with the query
                     {' '}
-                    <code className="p-1.5 bg-tertiary text-sm font-bold text-secondary rounded-md">
+                    <code className="p-1.5 bg-tertiary text-sm font-bold text-primary rounded-md">
                         spotify top 50 - {'{country}'}
                     </code>
                     . The songs displayed are retrieved from the best match of this query, which is far from accurate.
@@ -19,7 +19,7 @@ export default function TopListExplanation() {
                 <p className="text-lg text-secondary mt-2">
                     The accuracy for top tracks varies based on what country you aim to view. For instance, most European countries have official, or unofficial, playlists with their countries’ top songs. If you are unsure of the authenticity of the top songs, just check the ‘based on’ text underneath the last track. Tracks with the highest authenticity has 
                     {' '}
-                    <code className="p-1.5 bg-tertiary text-sm font-bold text-secondary rounded-md">
+                    <code className="p-1.5 bg-tertiary text-sm font-bold text-primary rounded-md">
                         Top 50 - {'{country}'}
                     </code>
                     {' '} 
