d67a0be1bbc8e498be69f2a2e28a841db34fc56c
Poxie <poxen12@hotmail.com>
2023-09-11 21:33:16 +0200

    Added gradient border top most popular top list tracks

--- components/top-lists/TopListPopularTracks.tsx
+++ components/top-lists/TopListPopularTracks.tsx
@@ -8,8 +8,8 @@ export default function TopListPopularTracks({ tracks, loading }: {
     loading: boolean;
 }) {
     return(
-        <div className="py-8 border-t-2 border-b-2 border-t-tertiary border-b-tertiary bg-secondary">
-            <ul className="w-main max-w-main mx-auto grid md:grid-cols-2 gap-2">
+        <div className="[--border-right:0] [--border-left:0] gradient-border bg-secondary">
+            <ul className="py-8 w-main max-w-main mx-auto grid md:grid-cols-2 gap-2">
                 {tracks?.map((track, key) => (
                     <TopListTrack 
                         loading={loading}
