1342de1a4033b76cbc51a465a6b13d2b70a627de
Poxie <poxen12@hotmail.com>
2023-09-13 11:33:27 +0200

    Changed history grid to flex box with scroll

--- components/track/index.tsx
+++ components/track/index.tsx
@@ -4,9 +4,11 @@ import SpotifyTrackImage from "../spotify-track-image";
 import { SpotifyTrack } from "@/types";
 import SpotifyTrackName from '../spotify-track-name';
 import SpotifyTrackArtists from '../spotify-track-artists';
+import clsx from 'clsx';
 
-export default function Track({ track }: {
+export default function Track({ track, className }: {
     track: SpotifyTrack | undefined;
+    className?: string;
 }) {
     if(!track) return(
         <div className="w-14 aspect-square">
@@ -15,7 +17,12 @@ export default function Track({ track }: {
     )
 
     return(
-        <div className="group flex items-start gap-3">
+        <div 
+            className={clsx(
+                "group flex items-start gap-3",
+                className,
+            )}
+        >
             <SpotifyTrackImage
                 imageClassName="min-w-[3.5rem] w-14 aspect-square rounded-md"
                 src={track.album.images.at(-1)?.url} 
--- contexts/preview/PreviewHistory.tsx
+++ contexts/preview/PreviewHistory.tsx
@@ -9,15 +9,12 @@ export default function PreviewHistory() {
     return(
         <div
             className={clsx(
-                "max-h-[70dvh] border-t-[1px] border-t-primary grid grid-rows-[0fr] overflow-hidden duration-500 transition-[grid-template-rows]",
+                "border-t-[1px] border-t-primary grid grid-cols-1 grid-rows-[0fr] duration-500 transition-[grid-template-rows]",
                 expanded && 'grid-rows-[1fr]',
             )}
         >
-            <div className={clsx(
-                "min-h-0 px-4 transition-[padding]",
-                expanded ? 'py-4 scrollbar-dark overflow-auto' : 'py-0',
-            )}>
-                <div className="mb-1 flex justify-between items-center text-xs">
+            <div className={"min-h-0 max-w-full duration-500 transition-[padding]"}>
+                <div className="mb-1 pt-4 px-4 flex justify-between items-center text-xs">
                     <span>
                         Playback history
                     </span>
@@ -31,16 +28,17 @@ export default function PreviewHistory() {
                     )}
                 </div>
                 {hasHistory ? (
-                    <div className="mt-2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8 gap-4">
+                    <div className="mt-2 px-4 pb-4 flex-1 flex gap-4 overflow-auto scrollbar-dark">
                         {history.map(track => (
                             <Track 
+                                className={"max-w-[200px]"}
                                 track={track}
                                 key={track.id}
                             />
                         ))}
                     </div>
                 ) : (
-                    <span className="block text-secondary text-xs">
+                    <span className="block px-4 pb-4 text-secondary text-xs">
                         Whops, there are no tracks in your history.
                     </span>
                 )}
