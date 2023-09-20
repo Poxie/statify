983ba292d9847d72816803263849ed257f456a7c
Poxie <poxen12@hotmail.com>
2023-09-20 06:38:23 +0200

    Updated buttons styles

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,3 +1,5 @@
+import { twMerge } from 'tailwind-merge';
+
 export default function Button({
     children,
     onClick,
@@ -12,7 +14,11 @@ export default function Button({
     return(
         <button
             onClick={onClick}
-            className={`transition-colors bg-c-primary hover:bg-c-primary-accent rounded-lg ${small ? 'py-[10px] px-3 text-xs' : 'py-[14px] px-4 text-sm'} ${className}`}
+            className={twMerge(
+                "transition-colors bg-c-primary hover:bg-c-primary-accent",
+                small ? 'py-[10px] px-3 text-xs rounded-md' : 'py-[14px] px-4 text-sm rounded-lg',
+                className,
+            )}
         >
             {children}
         </button>
