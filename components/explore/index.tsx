7f79793482ac4de3c4ce2ba5704021c03affc8cc
Poxie <poxen12@hotmail.com>
2023-09-20 05:09:15 +0200

    Using tailwind config to determine background color

--- components/explore/index.tsx
+++ components/explore/index.tsx
@@ -1,3 +1,9 @@
+"use client";
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../tailwind.config';
+
+const background = resolveConfig(tailwindConfig).theme?.backgroundColor?.primary;
+
 export default function Explore() {
     return(
         <main className="pt-20">
@@ -9,9 +15,14 @@ export default function Explore() {
                     Find songs you might like based on your favorite songs, artists, and genres!
                 </p>
             </div>
-            <div className="gradient-border [--border-left:0] [--border-right:0]">
+            <div 
+                className="gradient-border [--border-left:0] [--border-right:0]"
+                style={{ '--background': background } as React.CSSProperties}
+            >
                 <div className="py-8">
-                    
+                    <div className="w-[900px] max-w-main mx-auto">
+                        
+                    </div>
                 </div>
             </div>
         </main>
