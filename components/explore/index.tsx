9c2a67344f5358eb64254cf0b8e13bdfa5257bb6
Poxie <poxen12@hotmail.com>
2023-09-20 17:51:39 +0200

    Added hook to handle infinite scrolling

--- components/explore/index.tsx
+++ components/explore/index.tsx
@@ -3,37 +3,20 @@ import ExploreBasedOn from './ExploreBasedOn';
 import ExploreTracks from './ExploreTracks';
 import ExploreInputs from './ExploreInputs';
 import ExploreHeader from './ExploreHeader';
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import { SpotifyArtist, SpotifyTrack, SpotifyTrackWithColor } from '@/types';
-import { get } from "@/utils";
+import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
 
 export default function Explore() {
     const [tracks, setTracks] = useState<SpotifyTrack[]>([]);
     const [artists, setArtists] = useState<SpotifyArtist[]>([]);
     
-    const [recommendations, setRecommendations] = useState<SpotifyTrackWithColor[]>([]);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        if(!tracks.length && !artists.length) return setLoading(true);
-
-        const url = new URL(`${window.location.origin}/recommendations`);
-        url.searchParams.set('seed_artists', artists.map(artist => artist.id).join(','));
-        url.searchParams.set('seed_tracks', tracks.map(track => track.id).join(','));
-        
-        setLoading(true);
-
-        const abortController = new AbortController();
-        get<SpotifyTrackWithColor[]>(`${url.pathname}${url.search}`, abortController.signal).then(recommendations => {
-            setRecommendations(recommendations);
-            
-            setTimeout(() => {
-                setLoading(false);
-            }, 0)
-        })
-
-        return () => abortController.abort();
-    }, [artists.length, tracks.length]);
+    const url = new URL(`${window.location.origin}/recommendations`);
+    url.searchParams.set('seed_artists', artists.map(artist => artist.id).join(','));
+    url.searchParams.set('seed_tracks', tracks.map(track => track.id).join(','));
+    
+    const disabled = !artists.length && !tracks.length;
+    const { results, loading } = useInfiniteScroll<SpotifyTrackWithColor>(!disabled ? `${url.pathname}${url.search}` : '');
 
     const basedOnItems = useMemo(() => ([...artists, ...tracks]), [tracks.length, artists.length]);
     return(
@@ -48,7 +31,7 @@ export default function Explore() {
             <div className="w-main max-w-main mx-auto">
                 <ExploreBasedOn items={basedOnItems} />
                 <ExploreTracks 
-                    tracks={recommendations}
+                    tracks={results}
                     loading={loading}
                 />
             </div>
---
+++ hooks/useInfiniteScroll.ts
@@ -0,0 +1,54 @@
+import { get } from '@/utils';
+import { useCallback, useState, useEffect, useRef, RefObject } from 'react';
+
+const SCROLL_THRESHOLD = 200;
+export const useInfiniteScroll = <T>(query: string, scrollContainer: typeof window | RefObject<HTMLElement>=window) => {
+    const [loading, setLoading] = useState(true);
+    const [results, setResults] = useState<T[]>([]);
+    const fetching = useRef(false);
+
+    const fetchItems = useCallback(async (query: string, signal?: AbortSignal) => {
+        fetching.current = true;
+        const results = await get<T[]>(query, signal);
+        fetching.current = false;
+        return results;
+    }, [setLoading, setResults]);
+    useEffect(() => {
+        setLoading(true);
+        if(!query) return;
+
+        const abortController = new AbortController();
+        fetchItems(query, abortController.signal)
+            .then(results => {
+                setResults(results);
+                setLoading(false);
+            })
+
+        return () => abortController.abort();
+    }, [query]);
+
+    const scrollThresholdMet = useCallback(() => {
+        const container = 'current' in scrollContainer ? scrollContainer.current : scrollContainer;
+        if(!container) return;
+
+        const scroll = 'scrollHeight' in container ? container.scrollHeight - container.scrollTop - container.clientHeight : container.innerHeight + container.scrollY;
+        const height = 'innerHeight' in container ? document.body.offsetHeight : container.offsetHeight;
+        
+        return scroll + SCROLL_THRESHOLD >= height
+    }, [query]);
+    useEffect(() => {
+        const container = 'current' in scrollContainer ? scrollContainer.current : scrollContainer;
+
+        const onScroll = async () => {
+            if(scrollThresholdMet() && !fetching.current) {
+                const results = await fetchItems(query);
+                setResults(prev => [...prev, ...results]);
+            }
+        }
+
+        container?.addEventListener('scroll', onScroll);
+        return () => container?.removeEventListener('scroll', onScroll);
+    }, [query]);
+
+    return { loading, results };
+}
