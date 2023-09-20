4b33ca9cac0860d1b6ae5111d923159900f80d56
Poxie <poxen12@hotmail.com>
2023-09-20 18:24:37 +0200

    Scrolling to top and hiding overflow when all explore inputs are empty

--- components/explore/index.tsx
+++ components/explore/index.tsx
@@ -3,20 +3,35 @@ import ExploreBasedOn from './ExploreBasedOn';
 import ExploreTracks from './ExploreTracks';
 import ExploreInputs from './ExploreInputs';
 import ExploreHeader from './ExploreHeader';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import { SpotifyArtist, SpotifyTrack, SpotifyTrackWithColor } from '@/types';
 import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
 
 export default function Explore() {
     const [tracks, setTracks] = useState<SpotifyTrack[]>([]);
     const [artists, setArtists] = useState<SpotifyArtist[]>([]);
+    const disabled = !artists.length && !tracks.length;
     
     const url = new URL(`${window.location.origin}/recommendations`);
     url.searchParams.set('seed_artists', artists.map(artist => artist.id).join(','));
     url.searchParams.set('seed_tracks', tracks.map(track => track.id).join(','));
     
-    const disabled = !artists.length && !tracks.length;
     const { results, loading } = useInfiniteScroll<SpotifyTrackWithColor>(!disabled ? `${url.pathname}${url.search}` : '');
+    
+    // Scrolling to top and disabling overflow on deselect
+    if(disabled) {
+        document.body.style.overflow = 'hidden';
+        document.documentElement.style.scrollBehavior = 'smooth';
+        setTimeout(() => {
+            window.scrollTo({ top: 0 });
+            document.documentElement.style.scrollBehavior = '';
+        }, 0);
+    } else {
+        document.body.style.overflow = '';
+        document.body.style.scrollBehavior = '';
+    }
+    // Making sure overflow is returned to normal on unmount
+    useEffect(() => () => {document.body.style.overflow = ''}, [])
 
     const basedOnItems = useMemo(() => ([...artists, ...tracks]), [tracks.length, artists.length]);
     return(
