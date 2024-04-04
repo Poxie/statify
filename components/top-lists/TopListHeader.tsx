aec727883093336d7976c549a1030c11fd5979f4
Poxie <poxen12@hotmail.com>
2024-04-04 17:25:22 +0200

    Replace country search input

--- components/top-lists/TopListHeader.tsx
+++ components/top-lists/TopListHeader.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter, useSearchParams } from "next/navigation";
 import CustomSearchInput from "../custom-search-input";
+import ListInput from "../list-input";
 
 export default function TopListHeader() {
     const router = useRouter();
@@ -23,13 +24,10 @@ export default function TopListHeader() {
             <span className="block w-[600px] max-w-full mx-auto  mt-4 mb-6 sm:text-xl text-secondary">
                 It is time to leave your country’s bubble. This is a way to explore the culture of countries all around the world.
             </span>
-            <CustomSearchInput 
+            <ListInput 
                 path='/country/list'
                 placeholder={'Find your country...'}
-                onSelect={country => {
-                    if(country === 'global') return router.push('/top-lists');
-                    router.push(`/top-lists?country=${country}`, { scroll: false });
-                }}
+                onSelect={country => router.push(`/top-lists?country=${country}`)}
             />
         </div>
     )
