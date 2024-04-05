f44361f7cad3da9c8b90cd1cf503e106560fdb41
Poxie <poxen12@hotmail.com>
2024-04-05 23:21:15 +0200

    Use section element for profile artists

--- components/profile/ProfileArtists.tsx
+++ components/profile/ProfileArtists.tsx
@@ -15,45 +15,45 @@ export default function ProfileArtists() {
     const topArtists = artists.slice(0,3);
     const otherArtists = artists.slice(3, showAll ? artists.length : 3 + DEFAULT_VISIBLE_ARTISTS);
     return(
-        <>
-        <h2 className="mb-3 text-2xl md:text-3xl font-medium">
-            Your most liked artists
-        </h2>
-        <div className="border-[1px] border-tertiary rounded-md overflow-hidden">
-            <div className="p-7 grid md:grid-cols-3 items-end gap-7 md:gap-3 bg-dotted bg-[length:31px_31px] bg-center">
-                <ProfileTopArtist 
-                    artist={topArtists[1]}
-                    artistNumber={2}
-                />
-                <ProfileTopArtist 
-                    artist={topArtists[0]}
-                    artistNumber={1}
-                />
-                <ProfileTopArtist 
-                    artist={topArtists[2]}
-                    artistNumber={3} 
-                />
+        <section>
+            <h2 className="mb-3 text-2xl md:text-3xl font-medium">
+                Your most liked artists
+            </h2>
+            <div className="border-[1px] border-tertiary rounded-md overflow-hidden">
+                <div className="p-7 grid md:grid-cols-3 items-end gap-7 md:gap-3 bg-dotted bg-[length:31px_31px] bg-center">
+                    <ProfileTopArtist 
+                        artist={topArtists[1]}
+                        artistNumber={2}
+                    />
+                    <ProfileTopArtist 
+                        artist={topArtists[0]}
+                        artistNumber={1}
+                    />
+                    <ProfileTopArtist 
+                        artist={topArtists[2]}
+                        artistNumber={3} 
+                    />
+                </div>
+                <ul className="p-7 grid gap-3 grid-cols-2 sm:grid-cols-4 lg:grid-cols-7 bg-secondary">
+                    {loading && (
+                        Array.from(Array(DEFAULT_VISIBLE_ARTISTS)).map((_, key) => (
+                            <ProfileArtistSkeleton key={key} />
+                        ))
+                    )}
+                    {otherArtists.map((artist, index) => (
+                        <li key={artist.id}>
+                            <ProfileArtist 
+                                artist={artist}
+                                index={topArtists.length + index + 1}
+                            />
+                        </li>
+                    ))}
+                </ul>
             </div>
-            <ul className="p-7 grid gap-3 grid-cols-2 sm:grid-cols-4 lg:grid-cols-7 bg-secondary">
-                {loading && (
-                    Array.from(Array(DEFAULT_VISIBLE_ARTISTS)).map((_, key) => (
-                        <ProfileArtistSkeleton key={key} />
-                    ))
-                )}
-                {otherArtists.map((artist, index) => (
-                    <li key={artist.id}>
-                        <ProfileArtist 
-                            artist={artist}
-                            index={topArtists.length + index + 1}
-                        />
-                    </li>
-                ))}
-            </ul>
-        </div>
-        <ProfileSectionFooter 
-            showAll={showAll}
-            setShowAll={setShowAll} 
-        />
-        </>
+            <ProfileSectionFooter 
+                showAll={showAll}
+                setShowAll={setShowAll} 
+            />
+        </section>
     )
 }
