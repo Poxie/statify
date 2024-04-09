3a89952cb295734f751b3e305270ba2e06fde603
Poxie <poxen12@hotmail.com>
2024-04-09 15:22:52 +0200

    Add featured albums to artists

--- redux/slices/artists.ts
+++ redux/slices/artists.ts
@@ -1,4 +1,4 @@
-import { SpotifyAlbum, SpotifyTrack } from "@/types";
+import { SpotifyAlbum, SpotifyFeaturedAlbum, SpotifyTrack } from "@/types";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
@@ -6,6 +6,7 @@ const initialState: {
     [id: string]: Partial<{
         topTracks: SpotifyTrack[];
         topAlbums: SpotifyAlbum[];
+        featuredAlbums: SpotifyFeaturedAlbum[];
     }> | undefined;
 } = {};
 
@@ -32,11 +33,19 @@ const artistsSlice = createSlice({
                 ...prevArtist,
                 topAlbums: action.payload.topAlbums,
             }
-        }
+        },
+        setArtistFeaturedAlbums: (state, action: PayloadAction<{ artistId: string, featuredAlbums: SpotifyFeaturedAlbum[] }>) => {
+            const prevArtist = getPrevArtist(state, action.payload.artistId);
+
+            state[action.payload.artistId] = {
+                ...prevArtist,
+                featuredAlbums: action.payload.featuredAlbums,
+            }
+        },
     }
 })
 
-export const { setArtistTopTracks, setArtistTopAlbums } = artistsSlice.actions;
+export const { setArtistTopTracks, setArtistTopAlbums, setArtistFeaturedAlbums } = artistsSlice.actions;
 export default artistsSlice.reducer;
 
 // Hooks
@@ -45,4 +54,7 @@ export const selectArtistTopTracks = (state: RootState, artistId: string) => (
 );
 export const selectArtistTopAlbums = (state: RootState, artistId: string) => (
     state.artists[artistId]?.topAlbums
+);
+export const selectArtistFeaturedAlbums = (state: RootState, artistId: string) => (
+    state.artists[artistId]?.featuredAlbums
 );
